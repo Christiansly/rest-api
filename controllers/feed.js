7ef7f95587a56297cf12cbe45ee52e3c90f77dd0
Christiansly <abiode23@gmail.com>
2022-02-15 23:26:50 +0100

    first commit

---
+++ controllers/feed.js
@@ -0,0 +1,32 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is my post!",
+        imageUrl: "images/121271_1625035423.jpg",
+        creator: {
+          name: "Christian",
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Christian" },
+      createdAt: new Date(),
+    },
+  });
+};
