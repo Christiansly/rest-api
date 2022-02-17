04946536dc3c027fd0d63a0ff8fbf49ccc90c6da
Christiansly <abiode23@gmail.com>
2022-02-17 01:22:43 +0100

    Post Schema

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.use((req, res, next) => {
     next();
 });
 app.use('/feed', feedRoutes)
-mongoose.connect("mongodb+srv://admin:admin@cluster0.qfbbp.mongodb.net/feed?retryWrites=true&w=majority").then(() => console.log("Connected")).catch((err) => console.log(err))
+mongoose.connect("mongodb+srv://admin:admin@cluster0.qfbbp.mongodb.net/feed?retryWrites=true&w=majority").then(() => app.listen(8080)).catch((err) => console.log(err))
 app.listen(8080)
