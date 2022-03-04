9e85e2bca2500227bb67cbaf171b7213c5fe3544
Christiansly <abiode23@gmail.com>
2022-03-04 02:42:09 +0100

    update status api

--- controllers/status.js
+++ controllers/status.js
@@ -19,24 +19,24 @@ exports.getStatus = (req, res, next) => {
 }
 
 exports.updateStatus = (req, res, next) => {
-    const status = req.body.status
-    console.log(status)
-    if(!req.userId) {
-        const error = new Error('Status not available')
-        error.statusCode = 404
-        next(error)
-    }
-
-    User.findById(req.userId).then(user => {
-        user.status = status
-        return user.save()
-       
-    }).then(result => {
-        res.status(200).json({
-            status: status
-        })
-    }).catch(err => {
-        if (!err.statusCode) err.statusCode = 404
-        next(err)
-    })
+    const newStatus = req.body.status;
+    User.findById(req.userId)
+      .then(user => {
+        if (!user) {
+          const error = new Error('User not found.');
+          error.statusCode = 404;
+          throw error;
+        }
+        user.status = newStatus;
+        return user.save();
+      })
+      .then(result => {
+        res.status(200).json({ message: 'User updated.' });
+      })
+      .catch(err => {
+        if (!err.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err);
+      });
 }
--- routes/status.js
+++ routes/status.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const { body } = require("express-validator");
 const statusController = require("../controllers/status");
 const router = express.Router();
 const isAuth = require("../middleware/is-auth");
 
 router.get("/status", isAuth, statusController.getStatus);
-router.put(
-  "/status",
-  isAuth,
-//   [body("status").trim().not().isEmpty()],
-  statusController.updateStatus
-);
-
+router.patch(
+    '/status',
+    isAuth,
+    [
+      body('status')
+        .trim()
+        .not()
+        .isEmpty()
+    ],
+    statusController.updateStatus
+  );
+  
 module.exports = router;
